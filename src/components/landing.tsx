7429634df48317726ec37b49e7f2c21958fa64e5
onefarmer0x <onefarmer0x@gmail.com>
2025-02-21 12:40:37 +0200

    Initial Commit

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -14,39 +14,42 @@ const Landing: React.FC = () => {
 
   return (
     <div className="container-fluid main-bg p-0 m-auto d-flex flex-column min-vh-100">
+      <ParticleAnimation />
 
-    <ParticleAnimation />
-    
       {/* Desktop Background */}
       <div className="frame-wrapper">
-        <div className="container frame-content">
-          <div className="row">
+        <div className="container-fluid frame-content">
 
-            {/* Main Content - Desktop */}
-            <div className="container">
-              <div className="row">
-                <div className="col-12">
-                  <div className="video-wrapper">
-                    <div className="z-[1]">
-                      <video autoPlay loop muted playsInline className="">
-                        <source src="/videos/bg-home-1.mp4" type="video/mp4" />
-                      </video>
-                    </div>
-                  </div>
+           {/* Main Content - Video */}
+           <div className="video">
+            <div className="video-wrapper">
+                <div className="z-[1]">
+                  <video autoPlay loop muted playsInline className="">
+                    <source src="/videos/bg-home-1.mp4" type="video/mp4" />
+                  </video>
                 </div>
               </div>
+          </div>
 
-              <div className="row">
-                <div className="col-12">
-                  <div className="info-wrapper">
-                    <div className="row">
-                      <div className="col-lg-6 d-none d-lg-block">
+          {/* Main Content - BG */}
+          <div className="main-bg"></div>
+
+          <div className="row main-content">
+
+            {/* Main Content - Desktop */}
+            <div className="container-fluid">
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-12 col-lg-6">
+                    <div className="col-12">
+                      <div className="hero-wrapper">
                         <div className="description-section-wrapper">
-                          <div className="magic-container d-flex">
+                          <div className="magic-container">
+                            <p className="magic-text mb-3 m-auto">
+                              {"Not all wizards cast the same spells."}
+                            </p>
                             <p className="magic-text m-auto">
-                              {
-                                "CZARD is not just a memecoin—it’s an entire DeFi-powered ecosystem built on Binance Smart Chain (BSC), blending blockchain magic with real utility. The Market Mage protects HODLers, predicts market trends, and fights rug pulls with spells like Pump Spell, HODL Shield, and Rug Reveal."
-                              }
+                              {"Are you ready to experience CZARD's magic on BNB Chain?"}
                             </p>
                             <div className="sparkle"></div>
                             <div className="sparkle"></div>
@@ -54,79 +57,182 @@ const Landing: React.FC = () => {
                           </div>
                         </div>
                       </div>
-                      <div className="col-lg-6">
-                        <div className="contract-section-wrapper d-flex">
-                          <div className="contract-section m-auto">
-                            <div className="flex flex-col gap-4">
-                              {/* Contract Section */}
-                              <div className="flex w-full items-center gap-4">
-                                <div className="font-orb text-center">
-                                  <div className="magic-container d-flex">
-                                    <p className="magic-text m-auto">
-                                      CONTRACT
-                                    </p>
-                                    <div className="sparkle"></div>
-                                    <div className="sparkle"></div>
-                                    <div className="sparkle"></div>
-                                  </div>
-                                </div>
-                                <div className="border-primary-500 flex w-full items-center rounded-lg border border-solid p-[8px_16px]">
-                                  <a
-                                    href={`https://pancakeswap.finance/?outputCurrency=BNB&inputCurrency=${contractAddress}`}
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    className="contract-text"
-                                  >
-                                    {`${contractAddress.slice(
-                                      0,
-                                      4
-                                    )} ... ${contractAddress.slice(-4)}`}
-                                  </a>
-                                  <button
-                                    onClick={handleCopyAddress}
-                                    className="cursor-pointer active:scale-95"
-                                  >
-                                    <img
-                                      src="/icons/icon-copy.svg"
-                                      alt="Copy"
-                                    />
-                                  </button>
-                                </div>
+                    </div>
+                    <div className="col-12">
+                      <div className="roadmap-container-wrapper">
+                        <div className="roadmap-container">
+                          <div className="roadmap-title-wrapper">
+                            <div className="title">
+                              <div className="magic-container d-flex">
+                                <p className="magic-text m-auto">Roadmap</p>
+                                <div className="sparkle"></div>
+                                <div className="sparkle"></div>
+                                <div className="sparkle"></div>
                               </div>
+                            </div>
+                          </div>
+                          <div className="roadmap-wrapper">
+                            <div className="roadmap-points">
+                              <ul>
+                                <li>
+                                  <span className="roadmap-point magic-text">{`Wizard’s Prophecy`}</span>
+                                </li>
+                                <li>
+                                  <span className="roadmap-point magic-text">{`Agent As A Service - First Ever AaaS`}</span>
+                                </li>
+                                <li>
+                                  <span className="roadmap-point magic-text blur">{`Staking`}</span>
+                                </li>
+                                <li>
+                                  <span className="roadmap-point magic-text blur">{`Ambassador Program`}</span>
+                                </li>
+                                <li>
+                                  <span className="roadmap-point magic-text blur">{`DAO`}</span>
+                                </li>
+                                <li>
+                                  <span className="roadmap-point magic-text blur">{`NFT Collection`}</span>
+                                </li>
+                                <li>
+                                  <span className="roadmap-point magic-text blur">{`Play2Earn Games`}</span>
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-12 col-lg-6 px-0">
+                    <div className="row">
+                      {/* Spellbook Desktop */}
+                      <div className="col-12 px-0 d-none d-lg-block spellbook-container-wrapper">
+                        <div className="spellbook-wrapper desktop">
+                          <div className="spellbook-title-wrapper">
+                            <div className="title">
+                              <div className="magic-container d-flex">
+                                <p className="magic-text m-auto">Spellbook</p>
+                                <div className="sparkle"></div>
+                                <div className="sparkle"></div>
+                                <div className="sparkle"></div>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="spellbook-content">
+                            <div className="col-12">
+                              <div className="contract-section-wrapper d-flex">
+                                <div className="contract-section m-auto">
+                                  <div className="flex flex-col gap-4">
+                                    {/* Contract Section */}
+                                    <div className="flex w-full items-center gap-4">
+                                      <div className="font-orb text-center">
+                                        <div className="magic-container d-flex">
+                                          <p className="magic-text m-auto">
+                                            CONTRACT
+                                          </p>
+                                          <div className="sparkle"></div>
+                                          <div className="sparkle"></div>
+                                          <div className="sparkle"></div>
+                                        </div>
+                                      </div>
+                                      <div className="border-primary-500 flex w-full items-center rounded-lg border border-solid p-[8px_16px]">
+                                        <a
+                                          href={`https://pancakeswap.finance/?outputCurrency=BNB&inputCurrency=${contractAddress}`}
+                                          target="_blank"
+                                          rel="noreferrer"
+                                          className="contract-text"
+                                        >
+                                          {`${contractAddress.slice(
+                                            0,
+                                            4
+                                          )}...${contractAddress.slice(-4)}`}
+                                        </a>
+                                        <button
+                                          onClick={handleCopyAddress}
+                                          className="cursor-pointer active:scale-95"
+                                        >
+                                          <img
+                                            src="/icons/icon-copy.svg"
+                                            alt="Copy"
+                                          />
+                                        </button>
+                                      </div>
+                                    </div>
 
-                              {/* Social Links Section */}
-                              <div className="flex w-full items-center justify-between">
-                                <div className="magic-text">
-                                  <div className="magic-container d-flex">
-                                    <p className="magic-text m-auto">
-                                      Join the magic
-                                    </p>
-                                    <div className="sparkle"></div>
-                                    <div className="sparkle"></div>
-                                    <div className="sparkle"></div>
+                                    {/* Social Links Section */}
+                                    <div className="w-full items-center">
+
+                                      {/* X */}
+                                      <div className="social-item-wrapper">
+                                        <div className="magic-text">
+                                          <div className="magic-container d-flex">
+                                            <p className="magic-text m-auto">
+                                              X
+                                            </p>
+                                            <div className="sparkle"></div>
+                                            <div className="sparkle"></div>
+                                            <div className="sparkle"></div>
+                                          </div>
+                                        </div>
+                                        <a className="button-wrapper" href="https://x.com" target="_blank" rel="noopener noreferrer" >
+                                          <img src="/images/x.svg"  alt="X" />
+                                        </a>
+                                      </div>
+
+                                      {/* Telegram */}
+                                      <div className="social-item-wrapper">
+                                        <div className="magic-text">
+                                          <div className="magic-container d-flex">
+                                            <p className="magic-text m-auto">
+                                              Telegram
+                                            </p>
+                                            <div className="sparkle"></div>
+                                            <div className="sparkle"></div>
+                                            <div className="sparkle"></div>
+                                          </div>
+                                        </div>
+                                        <a className="button-wrapper" href="https://telegram.com" target="_blank" rel="noopener noreferrer" >
+                                          <img src="/images/telegram.svg"  alt="Telegram" />
+                                        </a>
+                                      </div>
+
+                                      {/* DexScreener */}
+                                      <div className="social-item-wrapper">
+                                        <div className="magic-text">
+                                          <div className="magic-container d-flex">
+                                            <p className="magic-text m-auto">
+                                              DexScreener
+                                            </p>
+                                            <div className="sparkle"></div>
+                                            <div className="sparkle"></div>
+                                            <div className="sparkle"></div>
+                                          </div>
+                                        </div>
+                                        <a className="button-wrapper" href="https://dexcreener.com" target="_blank" rel="noopener noreferrer" >
+                                          <img src="/images/dex.png"  alt="DexScreener" />
+                                        </a>
+                                      </div>
+
+                                      {/* Four.meme */}
+                                      <div className="social-item-wrapper">
+                                        <div className="magic-text">
+                                          <div className="magic-container d-flex">
+                                            <p className="magic-text m-auto">
+                                              four.meme
+                                            </p>
+                                            <div className="sparkle"></div>
+                                            <div className="sparkle"></div>
+                                            <div className="sparkle"></div>
+                                          </div>
+                                        </div>
+                                        <a className="button-wrapper" href="https://fourmeme.com" target="_blank" rel="noopener noreferrer" >
+                                          <img src="/images/fourmeme.webp"  alt="DexScreener" />
+                                        </a>
+                                      </div>
+                                      
+
+                                    
+                                    </div>
                                   </div>
-                                </div>
-
-                                <div className="ms-4 ms-lg-0 flex items-center">
-                                  <a
-                                    className="button-wrapper me-2 me-lg-4"
-                                    href="https://x.com"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                  >
-                                    <img src="/images/x.svg" alt="X" />
-                                  </a>
-                                  <a
-                                    className="button-wrapper"
-                                    href="https://telegram.com"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                  >
-                                    <img
-                                      src="/images/telegram.svg"
-                                      alt="Telegram"
-                                    />
-                                  </a>
                                 </div>
                               </div>
                             </div>
@@ -138,12 +244,10 @@ const Landing: React.FC = () => {
                 </div>
               </div>
             </div>
-            
           </div>
         </div>
       </div>
-
-      </div>
+    </div>
   );
 };
 
